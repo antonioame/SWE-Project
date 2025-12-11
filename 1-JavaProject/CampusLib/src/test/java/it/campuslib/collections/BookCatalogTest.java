7473afc5e26b515c908a50780c63d3c11cd8f3c3
Serena <s.fierro10@studenti.unisa.it>
2025-12-11 12:22:38 +0100

    Creazione classi di Test Book e BookCatalog

---
+++ 1-JavaProject/CampusLib/src/test/java/it/campuslib/collections/BookCatalogTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.campuslib.collections;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Seren
+ */
+public class BookCatalogTest {
+    
+    public BookCatalogTest() {
+    }
+
+    @Test
+    public void testAddBook() {
+    }
+
+    @Test
+    public void testRemoveBook() {
+    }
+
+    @Test
+    public void testSearchByTitle() {
+    }
+
+    @Test
+    public void testSearchByAuthor() {
+    }
+
+    @Test
+    public void testSearchByIsbn() {
+    }
+
+    @Test
+    public void testSearch() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testExportOnFile() {
+    }
+
+    @Test
+    public void testImportFromFile() {
+    }
+    
+}
---
+++ 1-JavaProject/CampusLib/src/test/java/it/campuslib/domain/catalog/BookTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.campuslib.domain.catalog;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Seren
+ */
+public class BookTest {
+    
+    public BookTest() {
+    }
+
+    @Test
+    public void testGetIsbn() {
+    }
+
+    @Test
+    public void testGetTitle() {
+    }
+
+    @Test
+    public void testGetAuthors() {
+    }
+
+    @Test
+    public void testGetPublishingYear() {
+    }
+
+    @Test
+    public void testGetCopies() {
+    }
+
+    @Test
+    public void testIsAdopted() {
+    }
+
+    @Test
+    public void testCheckAvailability() {
+    }
+
+    @Test
+    public void testSetTitle() {
+    }
+
+    @Test
+    public void testSetStatus() {
+    }
+
+    @Test
+    public void testSetPublishingYear() {
+    }
+
+    @Test
+    public void testSetCopies() {
+    }
+
+    @Test
+    public void testAddAuthor() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testCompareTo() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+    
+}
