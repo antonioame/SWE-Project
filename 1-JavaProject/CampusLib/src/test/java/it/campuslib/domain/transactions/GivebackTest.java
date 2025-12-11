5be8d279510eb1823b16ca15acbdd49f9745e458
Elena Collazzo <e.collazzo1@studenti.unisa.it>
2025-12-11 12:57:21 +0100

    Creazione classi di test Loan e Giveback

---
+++ 1-JavaProject/CampusLib/src/test/java/it/campuslib/domain/transactions/GivebackTest.java
@@ -0,0 +1,24 @@
+package it.campuslib.domain.transactions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class GivebackTest {
+    
+    public GivebackTest() {
+    }
+
+    @Test
+    public void testGiveBack() {
+    }
+
+    @Test
+    public void testGetEndDate() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+    
+}
