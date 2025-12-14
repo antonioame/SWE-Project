96953698cfbda5dde96d7f540adc43bcb4e28279
Lucacap5 <luca.capacchione347@gmail.com>
2025-12-14 18:48:42 +0100

    Correzione degli errori nelle classi User e UserTest

--- 1-JavaProject/CampusLib/src/main/java/it/campuslib/domain/users/User.java
+++ 1-JavaProject/CampusLib/src/main/java/it/campuslib/domain/users/User.java
@@ -79,7 +79,12 @@ public class User extends Person implements Comparable<User> {
      * @brief Imposta l'e-mail dell'utente.
      * @param[in] email E-mail utente.
      */
-    public void setEmail(String email) {
+    public void setEmail(String email) throws InvalidUserInfoException{
+        
+        if(!checkEmail(email)) {
+        
+            throw new InvalidUserInfoException();
+        }
         this.email = email;
     }
     
@@ -114,35 +119,13 @@ public class User extends Person implements Comparable<User> {
      */
     public LinkedList<Loan> getActiveLoans(LoanRegistry registry) {
         
-        if(registry == null) return null;
-        
         LinkedList<Loan> associatedLoans = new LinkedList<>();
         
+        if(registry == null) return associatedLoans;
+       
         associatedLoans = registry.searchByUser(this);
         
         return associatedLoans;
-        
-    }
-    
-    /**
-     * @brief Utilizza la lista dei suoi prestiti associati, e l'attributo maxLoans,
-     * per determinare il numero di prestiti che possono essere ancora effettuati.
-     * 
-     * @return Numero di prestiti ancora disponibili.
-     * @post Il numero di prestiti disponibili non deve essere minore di zero né
-     * maggiore del massimo numero di prestiti possibili.
-     * @see getActiveLoans()
-     */
-    public int getAvailableLoanSlots(LoanRegistry registry) {
-        
-        LinkedList<Loan> associatedLoans = this.getActiveLoans(registry);
-        
-        if(associatedLoans == null) return maxLoans;
-        
-        int numLoans = associatedLoans.size();
-        int avLoans = maxLoans - numLoans;
-        
-        return avLoans;
         
     }
     
@@ -152,12 +135,14 @@ public class User extends Person implements Comparable<User> {
      */
     public boolean canBorrow(LoanRegistry registry) {
         
-       int avLoans = getAvailableLoanSlots(registry);
-       
-       if(avLoans > 0) return true;
-       else return false;
+        LinkedList<Loan> associatedLoans = this.getActiveLoans(registry);
         
-    }
+        if(associatedLoans == null) return maxLoans > 0;
+        
+        int numLoans = associatedLoans.size();
+        
+        return ((maxLoans - numLoans) > 0);
+       }
     
 
     /**
